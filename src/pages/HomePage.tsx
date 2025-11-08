b3d58e301812aa857d37faa33f20aa051a19fa97
slai6-afk <slai6@pratt.edu>
2025-11-08 13:35:16 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,7 +37,7 @@ export function HomePage() {
       <Navigation />
 
       {/* Hero Section */}
-      <section className="pt-[100px] pb-[0px] px-6 md:px-12 lg:px-16 pr-[48px] pl-[48px]">
+      <section className="pt-[140px] pb-[0px] px-6 md:px-12 lg:px-16 pr-[48px] pl-[48px]">
         <div className="max-w-[1400px] mx-auto">
           <motion.div
             initial={{ opacity: 0, y: 40 }}
